269dcceefe70b31d0a145195c08f6bc77b9670c0
pansuriyaakshay <pansuriyaakshay28@gmail.com>
2023-10-25 15:08:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+  
 function App() {
+
+  const [data, setdata] = useState([]);
+  const [updateTitle, setUpdateTitle] = useState([]);
+  const [UpdateAuthor, setUpdateAuthor] = useState([]);
+
+  console.log(data);
+  
+  useEffect(() => {
+    axios.get("http://localhost:4000/posts").then((res) => {
+      console.log(res);
+      setdata(res.data);
+    });
+  }, []);
+
+  const AddData = (postId) => {  
+    axios.post('http://localhost:4000/posts/${postId}').then((res) => {
+      console.log(res.data);
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <input 
+        type='text' 
+        placeholder='update Title' 
+        value={updateTitle} 
+        onChange={(e) => setUpdateTitle(e.target.value)} 
+        />
+
+        <input 
+        type='text' 
+        placeholder='update Author' 
+        value={UpdateAuthor} 
+        onChange={(e) => setUpdateAuthor(e.target.value)} 
+        />
+
+        <button onClick={() => AddData}>Add</button>
+
+        {data.map((e) => {
+          return (
+            <>
+              <h1>{e.id}</h1>
+              <h1>{e.title}</h1>
+              <h1>{e.author}</h1>
+              <delete>Delete</delete>
+            </>
+          );
+        })}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
